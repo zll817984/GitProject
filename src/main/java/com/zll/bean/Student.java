39dd2a70fe60062ef170d7896a8984b59e87052b
zll817984 <644817984@qq.com>
2020-10-09 09:04:37 +0800

    添加了学生类

---
+++ src/main/java/com/zll/bean/Student.java
@@ -0,0 +1,13 @@
+package com.zll.bean;
+
+public class Student {
+    private String name;
+    private String id;
+    private int age;
+
+    public Student(String name, String id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+}
